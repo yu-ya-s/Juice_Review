44222a5da3331f8ba8df4f044c8ae9143d321984
__yuya <yuya1999.1130@me.com>
2020-10-19 19:41:59 +0900

    修正

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -27,6 +27,6 @@ class AppServiceProvider extends ServiceProvider
         Schema::defaultStringLength(191);
         if (\App::environment('production')) {
             \URL::forceScheme('https');
-        
+        }
     }
 }
