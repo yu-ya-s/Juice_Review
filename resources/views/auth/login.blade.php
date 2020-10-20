23f725c72f25a59f1e4120131e434e4b15a96985
__yuya <yuya1999.1130@me.com>
2020-10-20 18:04:11 +0900

    ログイン画面の日本語化

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -11,6 +11,14 @@
                     <form method="POST" action="{{ route('login') }}">
                         @csrf
 
+                        <div class="message" style = "text-align :center">
+                            <p>テストユーザーとして、<br>
+                                メールアドレス：test@example.com <br>
+                                パスワード：password <br>
+                                をお使いください。
+                            </p>
+                        </div>
+
                         <div class="form-group row">
                             <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
 
